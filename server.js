26fcbd7e2479bf5c4a9dfe2645409db663b293e7
earGO <rh.edwin@gmail.com>
2018-09-28 12:01:40 -0500

    register req added

--- server.js
+++ server.js
@@ -1,11 +1,71 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+            
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'bannanas',
+            entries: 0,
+            joined: new Date()
+            
+        }
+    ]
+}
+
+app.use(bodyParser.json());
+
 app.get('/', (req,res) => { 
-    res.send('this is working');
+    res.send(database.users);
+})
+
+app.post('/signin',(req,res) => {
+    if (req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+            res.json('success');
+        } else {
+            res.status(400).json('error logging in');
+        }
+})
+
+app.post('/register', (req,res) => {
+    const { email, name, password } = req.body;
+    
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+        
+    })
+    res.json(database.users[database.users.length-1]);
 })
 
 app.listen(3000,()=>{
     console.log('app is running on port 3000');
 })
+
+/*
+write down all routes on a server (startpoints on a frontend and endpoints on a backend)
+/ --> res = this is working
+/signin --> POST = success/fail - DONEEE
+/register --> POST = return new created user, new 'user' JSON object
+/profile/:userId --> GET = user
+/image --> PUT --> "user" rank count updated
+
+*/
